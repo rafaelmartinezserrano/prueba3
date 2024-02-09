746106423d3bdfffb5794ebd94d2f539a8618268
Rafa <rafa@postal6.academiapostal.es.local>
2024-02-09 14:15:37 +0100

    Añadida la clase Persona

--- src/codigo/Main.java
+++ src/codigo/Main.java
@@ -1,5 +1,7 @@
 package codigo;
 
 public class Main {
-
+	public static void main(String[] args) {
+		System.out.println("Hola Mundo");
+	}
 }
---
+++ src/codigo/Persona.java
@@ -0,0 +1,14 @@
+package codigo;
+
+public class Persona {
+
+	private String nombre;
+
+	public Persona(String nombre) {
+		super();
+		this.nombre = nombre;
+	}
+	
+	
+	
+}
